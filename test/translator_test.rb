789c3f0d2430f935563d75f646483207dad72c33
JP Lynch <JPLynch35@gmail.com>
2018-05-09 09:21:09 -0600

    Add test for eng_to_morse method

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,17 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+class TranslatorTest < Minitest::Test
+
+  def setup
+    @translator = Translator.new
+  end
+
+  def test_eng_to_morse
+    assert_output(/......-...-..--- .-----.-..-..-../) {@translator.eng_to_morse("Hello World")}
+  end
+
+
+
+end
