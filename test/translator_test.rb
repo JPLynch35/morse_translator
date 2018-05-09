7f17f319466c9a0009061c47ebb1f524e1a8c039
JP Lynch <JPLynch35@gmail.com>
2018-05-09 09:42:58 -0600

    Add test for from_file method

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/translator'
+require 'pry'
 
 class TranslatorTest < Minitest::Test
 
@@ -10,8 +11,11 @@ class TranslatorTest < Minitest::Test
 
   def test_eng_to_morse
     assert_output(/......-...-..--- .-----.-..-..-../) {@translator.eng_to_morse("Hello World")}
+    assert_output(/-......-.. .-.-.. ...-- ..........--..../) {@translator.eng_to_morse("There are 3 ships")}
   end
 
-
+  def test_translate_from_a_file
+    assert_output(/.. .--- ..-. .- ..-....-.../) {@translator.from_file("input.txt")}
+  end
 
 end
